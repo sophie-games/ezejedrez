166baaa4a2b50cc04972f5e9848fe7fdb59ed1fc
ezejerez99 <ezejerez99@gmail.com>
2021-03-31 19:59:53 -0300

    board-added

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Piece from "./src/ui/piece";
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Board from './src/ui/board.jsx';
+import { Dimensions } from 'react-native';
+
+const window = Dimensions.get('window');
+const screen = Dimensions.get('screen');
 
 export default function App() {
+  const [dimensions, setDimensions] = useState({ window, screen });
+
+  const onChange = ({ window, screen }) => {
+    setDimensions({ window, screen });
+  };
+
+  useEffect(() => {
+    Dimensions.addEventListener('change', onChange);
+    return () => {
+      Dimensions.removeEventListener('change', onChange);
+    };
+  }, []);
+
+  const windowWidth = dimensions.window.width;
+  const windowHeight = dimensions.window.height;
+  const boardSize = windowWidth < windowHeight ? windowWidth : windowHeight;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Board size={boardSize} />
 
-      <Piece piece="w_pawn" />
-
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -18,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });

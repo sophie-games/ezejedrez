45cfdf7b0bb2dfdd4a7e4fc140670a4116209357
Leo Massaroli <massaroli.leonardo@gmail.com>
2021-04-21 17:14:20 -0300

    fix columns / rows order - board

--- src/ui/board.jsx
+++ src/ui/board.jsx
@@ -18,17 +18,20 @@ export default function Board({ size = 500, board }) {
         height: size,
         display: 'flex',
         flexDirection: 'column-reverse', // "column-reverse" because the (0,0) square is the one at the bottom left.
-      }}>
-      {columnArray.map((c) => (
-        <View key={c} style={styles.row}>
-          {rowArray.map((r) => {
+      }}
+    >
+      {rowArray.map((r) => (
+        <View key={r} style={styles.row}>
+          {columnArray.map((c) => {
             const boardPiece = board.find((p) => p.x === c && p.y === r);
+
+            const pieceColorCode = boardPiece.color === 'white' ? 'w' : 'b';
+
             const piece = boardPiece
-              ? `${boardPiece.color === 'white' ? 'w' : 'b'}_${
-                  boardPiece.pieceType
-                }`
+              ? `${pieceColorCode}_${boardPiece.pieceType}`
               : null;
-            return <Square board={board} piece={piece} key={r} x={r} y={c} />;
+
+            return <Square board={board} piece={piece} key={c} x={c} y={r} />;
           })}
         </View>
       ))}

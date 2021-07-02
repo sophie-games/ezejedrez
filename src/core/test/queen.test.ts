1290a6d7b5d5db27bd26f0c0956eedd1612c2ffa
ezejerez99 <ezejerez99@gmail.com>
2021-07-02 18:58:17 -0300

    wipa

--- src/core/test/queen.test.ts
+++ src/core/test/queen.test.ts
@@ -1,32 +1,32 @@
-import Chess from "../chess";
-import Queen from "../pieces/queen";
-import Doge from "../pieces/doge";
+import Chess from '../chess';
+import Queen from '../pieces/queen';
+import Doge from '../pieces/doge';
 
-describe("Queen suite", () => {
-  test("TODO: Queen should be in its correct place", () => {
+describe('Queen suite', () => {
+  test('Queen should be in its correct place', () => {
     const chess = new Chess();
     const arrayBoard = chess.getBoardAsArray();
     const whiteQueen = chess.getPiece(3, 0);
     const blackQueen = chess.getPiece(3, 7);
 
-    expect(whiteQueen.pieceType).toBe("queen");
-    expect(whiteQueen.color).toBe("white");
+    expect(whiteQueen.pieceType).toBe('queen');
+    expect(whiteQueen.color).toBe('white');
 
-    expect(blackQueen.pieceType).toBe("queen");
-    expect(blackQueen.color).toBe("black");
+    expect(blackQueen.pieceType).toBe('queen');
+    expect(blackQueen.color).toBe('black');
 
     expect(
-      arrayBoard.filter((piece) => piece && piece.pieceType === "queen").length
+      arrayBoard.filter((piece) => piece && piece.pieceType === 'queen').length,
     ).toBe(2);
   });
 
-  describe(".getMovements(x,y)", () => {
-    test("The queen can be moved any number of unoccupied squares in a straight line vertically, horizontally, or diagonally", () => {
+  describe('.getMovements(x,y)', () => {
+    test('The queen can be moved any number of unoccupied squares in a straight line vertically, horizontally, or diagonally', () => {
       const chess = new Chess();
 
       chess.cleanBoard();
 
-      chess.addPiece(new Queen("white"), 4, 3);
+      chess.addPiece(new Queen('white'), 4, 3);
 
       const movements = chess.getPieceMovements(4, 3);
 
@@ -76,36 +76,36 @@ describe("Queen suite", () => {
       expect(movements.length).toBe(27);
     });
 
-    test("The Queen cannot overstep ally pieces", () => {
+    test('The Queen cannot overstep ally pieces', () => {
       const chess = new Chess();
 
       chess.cleanBoard();
 
-      chess.addPiece(new Queen("white"), 4, 3);
+      chess.addPiece(new Queen('white'), 4, 3);
 
       // Top
-      chess.addPiece(new Doge("white"), 4, 7);
+      chess.addPiece(new Doge('white'), 4, 7);
 
       // Bottom
-      chess.addPiece(new Doge("white"), 4, 1);
+      chess.addPiece(new Doge('white'), 4, 1);
 
       // Left
-      chess.addPiece(new Doge("white"), 1, 3);
+      chess.addPiece(new Doge('white'), 1, 3);
 
       // Right
-      chess.addPiece(new Doge("white"), 7, 3);
+      chess.addPiece(new Doge('white'), 7, 3);
 
       // Forward top left
-      chess.addPiece(new Doge("white"), 2, 5);
+      chess.addPiece(new Doge('white'), 2, 5);
 
       // Forward top right
-      chess.addPiece(new Doge("white"), 6, 5);
+      chess.addPiece(new Doge('white'), 6, 5);
 
       // Backward bottom left
-      chess.addPiece(new Doge("white"), 2, 1);
+      chess.addPiece(new Doge('white'), 2, 1);
 
       // Backward bottom right
-      chess.addPiece(new Doge("white"), 6, 1);
+      chess.addPiece(new Doge('white'), 6, 1);
 
       const movements = chess.getPieceMovements(4, 3);
 
@@ -140,44 +140,44 @@ describe("Queen suite", () => {
       expect(movements.length).toBe(12);
     });
 
-    test("The Queen cannot overstep enemy pieces, only capture the first ones that cross it in each direction", () => {
+    test('The Queen cannot overstep enemy pieces, only capture the first ones that cross it in each direction', () => {
       const chess = new Chess();
 
       chess.cleanBoard();
 
-      chess.addPiece(new Queen("white"), 4, 3);
+      chess.addPiece(new Queen('white'), 4, 3);
 
       // Top
-      chess.addPiece(new Doge("black"), 4, 7);
-      chess.addPiece(new Doge("black"), 4, 6);
+      chess.addPiece(new Doge('black'), 4, 7);
+      chess.addPiece(new Doge('black'), 4, 6);
 
       // Bottom
-      chess.addPiece(new Doge("black"), 4, 2);
-      chess.addPiece(new Doge("white"), 4, 1);
+      chess.addPiece(new Doge('black'), 4, 2);
+      chess.addPiece(new Doge('white'), 4, 1);
 
       // Left
-      chess.addPiece(new Doge("white"), 2, 3);
-      chess.addPiece(new Doge("black"), 1, 3);
+      chess.addPiece(new Doge('white'), 2, 3);
+      chess.addPiece(new Doge('black'), 1, 3);
 
       // Right
-      chess.addPiece(new Doge("black"), 6, 3);
-      chess.addPiece(new Doge("white"), 7, 3);
+      chess.addPiece(new Doge('black'), 6, 3);
+      chess.addPiece(new Doge('white'), 7, 3);
 
       // Forward top left
-      chess.addPiece(new Doge("black"), 1, 6);
-      chess.addPiece(new Doge("black"), 2, 5);
+      chess.addPiece(new Doge('black'), 1, 6);
+      chess.addPiece(new Doge('black'), 2, 5);
 
       // Forward top right
-      chess.addPiece(new Doge("black"), 7, 6);
-      chess.addPiece(new Doge("black"), 6, 5);
+      chess.addPiece(new Doge('black'), 7, 6);
+      chess.addPiece(new Doge('black'), 6, 5);
 
       // Backward bottom left
-      chess.addPiece(new Doge("black"), 1, 0);
-      chess.addPiece(new Doge("white"), 2, 1);
+      chess.addPiece(new Doge('black'), 1, 0);
+      chess.addPiece(new Doge('white'), 2, 1);
 
       // Backward bottom right
-      chess.addPiece(new Doge("black"), 7, 0);
-      chess.addPiece(new Doge("black"), 6, 1);
+      chess.addPiece(new Doge('black'), 7, 0);
+      chess.addPiece(new Doge('black'), 6, 1);
 
       const movements = chess.getPieceMovements(4, 3);
 

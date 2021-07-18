9fb220b5264d344bca26b91825f30a21825e196c
Leonardo Massaroli <leonardo.massaroli@globant.com>
2021-07-18 14:39:04 -0300

    fix space

--- src/core/chess.ts
+++ src/core/chess.ts
@@ -72,7 +72,7 @@ export default class Chess {
       }
     }
 
-    //Adding pawns
+    // Adding pawns
     const pawnLines = [
       { lineY: 1, color: 'white' },
       { lineY: 6, color: 'black' },
